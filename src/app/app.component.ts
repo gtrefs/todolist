dc98d5429c37622165dfb33edd787a5e8faa7872
Gregor Trefs <Gregor.Trefs@gmail.com>
2019-01-04 16:58:40 +0100

    A new component #4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1> Welcome to {{title}}</h1>`,
+  template: `<h1> Welcome to {{title}}</h1>
+  <app-input-button-unit></app-input-button-unit>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
